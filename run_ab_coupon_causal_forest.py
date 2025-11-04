edee5b80bf920d3b91fae8672a82efafbee555a5
Yue Fei <yuefei9943@gmail.com>
2025-11-04 11:17:10 -0500

    Added step 1 and step 2 for ab testing; Added some results

--- run_ab_coupon_causal_forest.py
+++ run_ab_coupon_causal_forest.py
@@ -47,18 +47,20 @@ feature_cols = [
     "coupon_per_purchase", "activity_score", "price_sensitivity", "user_os_iOS"
 ]
 X = df[feature_cols].values
-Y = df["is_add"].astype(float).values
+Y = df["is_add"].astype(int).values
 
 # ========== 1. Popup main effect ==========
 print("\n>>> Estimating Popup (A) effect >>>")
 df_A = df[df["group"].isin([0, 1, 2])].copy()
 X_A = df_A[feature_cols].values
-Y_A = df_A["is_add"].astype(float).values
+Y_A = df_A["is_add"].astype(int).values
 T_A = df_A["A_popup"].fillna(0).astype(int).values
 
 cf_A = CausalForestDML(
     model_y=RandomForestRegressor(n_estimators=100, max_depth=6, random_state=RANDOM_STATE),
     model_t=RandomForestClassifier(n_estimators=100, max_depth=6, random_state=RANDOM_STATE),
+    discrete_outcome=True,
+    discrete_treatment=True,
     n_estimators=1000, random_state=RANDOM_STATE
 )
 cf_A.fit(Y_A, T_A, X=X_A)
@@ -66,6 +68,9 @@ tau_A = cf_A.effect(X_A)
 df_A["tau_A"] = tau_A
 ate_A = cf_A.ate(X_A)
 p_A = ttest_1samp(tau_A, 0).pvalue
+
+ate_A = float(ate_A)
+p_A = float(p_A)
 print(f"ATE(Popup vs Banner) = {ate_A:.4f}, p = {p_A:.3e}")
 
 # Save and plot
@@ -79,16 +84,19 @@ plt.tight_layout()
 plt.savefig(os.path.join(OUTPUT_DIR, "cate_popup.png"), dpi=300)
 plt.close()
 print("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
+
 # ========== 2️. Coupon main effect ==========
 print("\n>>> Estimating Coupon (B) effect >>>")
 df_B = df[df["group"].isin([1, 2])].copy()
 X_B = df_B[feature_cols].values
-Y_B = df_B["is_add"].astype(float).values
+Y_B = df_B["is_add"].astype(int).values
 T_B = df_B["B_coupon"].astype(int).values
 
 cf_B = CausalForestDML(
     model_y=RandomForestRegressor(n_estimators=100, max_depth=6, random_state=RANDOM_STATE),
     model_t=RandomForestClassifier(n_estimators=100, max_depth=6, random_state=RANDOM_STATE),
+    discrete_outcome=True,
+    discrete_treatment=True,
     n_estimators=1000, random_state=RANDOM_STATE
 )
 cf_B.fit(Y_B, T_B, X=X_B)
@@ -96,6 +104,9 @@ tau_B = cf_B.effect(X_B)
 df_B["tau_B"] = tau_B
 ate_B = cf_B.ate(X_B)
 p_B = ttest_1samp(tau_B, 0).pvalue
+
+ate_B = float(ate_B)
+p_B = float(p_B)
 print(f"ATE(Coupon vs Non-Coupon) = {ate_B:.4f}, p = {p_B:.3e}")
 
 # Save and plot
@@ -111,4 +122,4 @@ plt.close()
 print("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
 
 
-# ========== 3️⃣ Investigate the interaction of popup and coupon ==========
+# ========== 3. Investigate the interaction of popup and coupon ==========
