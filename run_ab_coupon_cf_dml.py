671ec7a6fdc8a3685c6e951e0d6aeffe811c54ed
Yue Fei <yuefei9943@gmail.com>
2025-11-12 18:47:02 -0500

    Updated CF DML for three arm A/B hypothesis testing

--- run_ab_coupon_cf_dml.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-from causalml.metrics import auuc_socre, plot_gain
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
--- run_ab_coupon_cfdml.py
+++ run_ab_coupon_cfdml.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from io import StringIO
-from econml.dml import CausalForestDML
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
@@ -9,12 +8,19 @@ from sklearn.pipeline import Pipeline
 import numpy as np
 import matplotlib.pyplot as plt
 
+import warnings
+warnings.filterwarnings('ignore')
+from econml.dml import CausalForestDML
+from econml.orf import DMLOrthoForest, DROrthoForest
+from econml.cate_interpreter import SingleTreeCateInterpreter
+from econml.sklearn_extensions.linear_model import WeightedLassoCVWrapper
+
 plt.rcParams['font.sans-serif'] = ['DejaVu Sans']
 plt.rcParams['axes.unicode_minus'] = False
 np.random.seed(42)
 # === Step 1 ===
 df = pd.read_csv("user_data.csv")
-df.describe()
+print(df.describe())
 # === Step 2 ===
 # Tag
 C = ["Userid"]
@@ -69,7 +75,7 @@ Y_arr = df_Y.values.ravel()
 T_arr = df["group_code"].values                   
 W_arr = W_proc                 
 
-print("Shapes before split:")
+print("\n>>> Shapes before split: >>>")
 print("X:", df_X.shape)
 print("Y:", df_Y.shape)
 print("T:", np.shape(T_arr))
@@ -78,30 +84,48 @@ X_train, X_test, Y_train, Y_test, T_train, T_test, W_train, W_test = train_test_
     X_arr, Y_arr, T_arr, W_arr, test_size=0.3, random_state=42
 )
 
-cf_model = CausalForestDML(
-    model_y=RandomForestRegressor(n_estimators=100, max_depth=5),
+# To better understand how balanced my experimental/causal-inference setup is:
+print("\n>>> Checking Treatment Class Balance >>>")
+unique, counts = np.unique(T_train, return_counts=True)
+for u, c in zip(unique, counts):
+    print(f"Treatment {u}: {c} samples ({c / len(T_train):.2%})")
+
+# === Compare T0 and T1 ===
+mask_01 = (T_train == 0) | (T_train == 1)
+cf_01 = CausalForestDML(
     model_t=RandomForestClassifier(n_estimators=100, max_depth=5),
+    model_y=RandomForestRegressor(n_estimators=100, max_depth=5),
     n_estimators=100,
     min_samples_leaf=20,
     discrete_treatment=True,
+    discrete_outcome=True,
     random_state=42
 )
-cf_model.fit(Y_train, T_train, X=X_train, W=W_train)
+te_01_pred = cf_01.effect(X_test)
+te_01_lower, te_01_upper = cf_01.effect_interval(X_test, alpha=0.05)
+print(f"Average Treatment Effect (ATE): {te_01_pred.mean():.2f}")
+print(f"Treatment Effect Std-Dev: {te_01_pred.std():.2f}")
+print(f"Treatment Effect Min: {te_01_pred.min():.2f}")
+print(f"Treatment Effect Max: {te_01_pred.max():.2f}")
+cf_01.fit(Y_train[mask_01], T_train[mask_01], X=X_train[mask_01])
+print("\n <<<<<<<< CausalForestDML (T0 vs T1) Training Completed !!! <<<<<<<<<<<< \n")
 
-te_pred = cf_model.effect(X_test)
-te_lower, te_upper = cf_model.effect_interval(X_test, alpha=0.5)
-
-print(f"平均处理效应（ATE）: {te_pred.mean():.2f}")
-print(f"处理效应标准差: {te_pred.std():.2f}")
-print(f"最小处理效应: {te_pred.min():.2f}")
-print(f"最大处理效应: {te_pred.max():.2f}")
-
-
-plt.figure(figsize=(12,5))
-
-plt.hist(te_pred, bins=50, alpha=0.7, color='blue', edgecolor='black')
-plt.axvline(te_pred.mean(), color='red', linestyle='--', label=f'Mean Effect: {te_pred.mean():.2f}')
-plt.xlabel('Treatment Effect')
-plt.ylabel('Frequency')
-plt.title('Individual Treatment Effect (ITE) Distribution')
-plt.legend()
+# === Compare T0 and T2 ===
+mask_02 = (T_train == 0) | (T_train == 2)
+cf_02 = CausalForestDML(
+    model_t=RandomForestClassifier(n_estimators=100, max_depth=5),
+    model_y=RandomForestRegressor(n_estimators=100, max_depth=5),
+    n_estimators=100,
+    min_samples_leaf=20,
+    discrete_treatment=True,
+    discrete_outcome=True,
+    random_state=42
+)
+cf_02.fit(Y_train[mask_02], T_train[mask_02], X=X_train[mask_02])
+print("\n <<<<<<<< CausalForestDML (T0 vs T2) Training Completed !!! <<<<<<<<<<<< \n")
+te_02_pred = cf_02.effect(X_test)
+te_02_lower, te_02_upper = cf_02.effect_interval(X_test, alpha=0.05)
+print(f"Average Treatment Effect (ATE): {te_02_pred.mean():.2f}")
+print(f"Treatment Effect Std-Dev: {te_02_pred.std():.2f}")
+print(f"Treatment Effect Min: {te_02_pred.min():.2f}")
+print(f"Treatment Effect Max: {te_02_pred.max():.2f}")
